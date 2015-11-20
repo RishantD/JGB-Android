2f1fb9ada9d7bac54fe3fb399c01aeb8cb1e9658
Rishant Dwivedi <Rishant Dwivedi>
2015-11-19 18:24:54 -0600

    Next

---
+++ app/src/main/java/com/jesusgandhiandbebe/tests/testLobby.java
@@ -0,0 +1,14 @@
+package com.jesusgandhiandbebe.tests;
+
+/**
+ * Created by rishant_dwivedi on 11/17/15.
+ */
+public class testLobby {
+    // Test Creation of lobby with user input
+        // Create user input
+    // Test Creation without user input
+        // Create non users
+    // Test Lobby get and show
+        // Get lobby
+    // Test add users to lobby
+}
